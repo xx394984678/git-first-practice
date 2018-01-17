47cf266ea3530168ad1012766c05aa512d89aff3
lirui <394984678@qq.com>
2018-01-17 11:28:54 +0800

    在master上更改，但这次没冲突

--- app/src/main/java/git/lirui/practice/SettingsActivity.java
+++ app/src/main/java/git/lirui/practice/SettingsActivity.java
@@ -86,6 +86,9 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
     };
 
+
+
+    
     /**
      * Helper method to determine if the device has an extra-large screen. For
      * example, 10" tablets are extra-large.
