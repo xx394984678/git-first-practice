513578d2b17db4e38156b83ea136c54e4b86164d
lirui <394984678@qq.com>
2018-01-17 10:50:54 +0800

    手贱，更改了一次

--- app/src/main/java/git/lirui/practice/AppCompatPreferenceActivity.java
+++ app/src/main/java/git/lirui/practice/AppCompatPreferenceActivity.java
@@ -12,6 +12,10 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+
+
+
 /**
  * A {@link android.preference.PreferenceActivity} which implements and proxies the necessary calls
  * to be used with AppCompat.
