faf3dd70a2f4f8f0c444d3fa6df3187cb89cb64c
lirui <394984678@qq.com>
2018-05-09 10:08:32 +0800

    建立框架

---
+++ app/src/main/java/git/lirui/practice/api/ApiConstant.java
@@ -0,0 +1,9 @@
+package git.lirui.practice.api;
+
+/**
+ * Created by mayuming on 2018/5/9.
+ */
+
+public class ApiConstant {
+    public static final String SIGN = "DEVELOP";
+}
---
+++ app/src/main/java/git/lirui/practice/fragment/TestActivity.java
@@ -0,0 +1,26 @@
+package git.lirui.practice.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import git.lirui.practice.R;
+import git.lirui.practice.api.ApiConstant;
+
+/**
+ * Created by mayuming on 2018/5/9.
+ */
+
+public class TestActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_layout);
+        textView = findViewById(R.id.sign);
+
+        textView.setText(ApiConstant.SIGN);
+    }
+}
